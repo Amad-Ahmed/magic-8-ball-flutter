6a21dacec57da51e96e1d20a41ceeba2ef4eb683
Amad-Ahmed <asiddiqui.bscs20seecs@seecs.edu.pk>
2022-10-09 13:09:44 +0500

    Ball project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text('Magic Ball'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Ball(),
+    ),
+  ));
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int answer = 1;
+  void randomAnswer() {
+    setState(() {
+      answer = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          randomAnswer();
+        },
+        child: Image.asset('images/ball$answer.png'),
       ),
     );
+  }
+}
